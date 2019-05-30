f3124e1d2eac1470c4dd128c6b354fd9345a0d9b
Nate Magera <nmagera07@gmail.com>
2019-05-30 19:20:22 -0400

    fixed further styling issues

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -32,10 +32,9 @@ class TodoForm extends React.Component {
             onChange={this.handleChanges}
             placeholder="Enter Todo Item"
           />
-          
+          <button class="first-btn">Add Todo</button>
         </form>
         <div className="btn">
-            <button class="first-btn">Add Todo</button>
         <button onClick={this.props.deleteItem} >
           Clear Completed
         </button>
